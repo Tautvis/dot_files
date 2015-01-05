1c6e000f7293a3f6a4b7acdd172734f0ae52043a
Tautvydas <tautvydas.misiunas@gmail.com>
2015-01-05 20:08:06 +0000

    Update get_libs.sh

--- dot_files/get_libs.sh
+++ dot_files/get_libs.sh
@@ -1,1 +1,27 @@
-#TEST
+# Download some common libraries and set them up
+BIN_DIR=$HOME/bin
+TPM_DIR=$HOME/tpm_setup
+
+if [ ! -d "$BIN_DIR" ]; then
+  mkdir $BIN_DIR
+fi
+
+if [ ! -d "$TPM_DIR" ]; then
+  mkdir $TPM_DIR
+fi
+
+
+mkdir $TPM_DIR
+
+
+# jq
+wget -P $TPM_DIR/ http://stedolan.github.io/jq/download/linux64/jq
+mv $TPM_DIR/jq $BIN_DIR
+chmod +x $BIN_DIR/jq
+
+
+
+
+
+# Clean up
+rm $TPM_DIR -r
